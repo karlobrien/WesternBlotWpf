d2277c226a4523754d31a088410e2fd7aaf2c2ed
Karl OBrien <karlobrien@gmail.com>
2015-06-15 19:54:50 +0100

    Add mahapp theme

--- WesternBlotWpf/App.xaml.cs
+++ WesternBlotWpf/App.xaml.cs
@@ -11,7 +11,7 @@ namespace WesternBlotWpf
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
         public App()
         {
--- WesternBlotWpf/ShellViewModel.cs
+++ WesternBlotWpf/ShellViewModel.cs
@@ -1,3 +1,29 @@
+using System.Windows;
+
 namespace WesternBlotWpf {
-    public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell { }
+    public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                NotifyOfPropertyChange(() => Name);
+                NotifyOfPropertyChange(() => CanSayHello);
+            }
+        }
+
+        public bool CanSayHello
+        {
+            get { return !string.IsNullOrWhiteSpace(Name); }
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show(string.Format("Hello {0}!", Name)); //Don't do this in real life :)
+        }
+    }
 }
